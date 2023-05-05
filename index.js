5c00fbc985d2163cbbe6fd7f54ce70058a7b7ce1
Zeyna Baaj <zainabaaj@gmail.com>
2023-05-05 17:39:57 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(x = "roller-skate") {
+    return `This Saturday, I want to ${x}!`
+}
+
+function mondayWork(x = "go to the office") {
+    return `This Monday, I will ${x}.`
+}
+
+function wrapAdjective(x = "*") {
+    let adj = "special"
+    return function (adj) {
+        return `You are ${x}${adj}${x}!`
+    }
+}
